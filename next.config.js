ed179607a6a9e49fb71ee98db4afad1f32ec7020
EOMZON <116791118+EOMZON@users.noreply.github.com>
2023-11-14 15:15:46 +0800

    20231114 repo prefix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
+//20231114 不加repo前缀github pages无法加载资源
 /** @type {import('next').NextConfig} */
+
+const repo = 'my-portfolio';
+const assetPrefix = `/${repo}/`;
+const basePath = `/${repo}`;
+
 const nextConfig = {
+  basePath,
+  assetPrefix,
   output: 'export',
 };
 
