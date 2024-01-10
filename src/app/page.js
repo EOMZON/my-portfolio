5cefd0de6b6ea6bafeeb2bf0a1b44845dad6b8f8
EOMZON <116791118+EOMZON@users.noreply.github.com>
2024-01-10 17:39:23 +0800

    20240110 use className instead class

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,11 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main>
-      <div class="flex items-center justify-center text-3xl  mt-20">
-        <h1 class="text-5xl font-bold">Portfolio test</h1>
+      <div className="flex items-center justify-center text-3xl  mt-20">
+        <h1 className="text-5xl font-bold">Portfolio test</h1>
       </div>
-      <div class="flex items-center justify-center text-3xl  mt-10">
-        <h1 class="text-1xl ">Portfolio test</h1>
+      <div className="flex items-center justify-center text-3xl  mt-10">
+        <h1 className="text-1xl ">Portfolio test</h1>
       </div>
     </main>
   );
