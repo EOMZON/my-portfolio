a9e6c613e6f1d7451ff22aa6ba77689031da252f
EOMZON <116791118+EOMZON@users.noreply.github.com>
2024-01-11 19:41:14 +0800

    20240111 Head Comp

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
-import { greetingsText } from './texts';
-
 export const Header = () => {
+  const greetingsText =
+    "👋 Hi there,I'm Zon, Product Designer with a touch of magic 🔮 on a mission to help you level up your value 🚀";
   return (
     <div className="flex items-center justify-center   mt-20 ml-12">
       <h1 className="text-5xl ">{greetingsText}</h1>
