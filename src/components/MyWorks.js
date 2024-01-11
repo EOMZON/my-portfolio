6959c5509e65b05501ac26996bc29c7f0ab0ea3f
EOMZON <116791118+EOMZON@users.noreply.github.com>
2024-01-11 19:54:41 +0800

    20240111 chage pic format

--- src/components/MyWorks.js
+++ src/components/MyWorks.js
@@ -1,24 +1,9 @@
 import { Section } from './Section';
-// import Image from 'next/image';
-// const ImageComponent = () => {
-//   return (
-//     <div>
-//       <Image
-//         src="/pjHomeCover.svg"
-//         alt="Next.js Logo"
-//         width={1198}
-//         height={656}
-//         priority
-//         className="mt-4"
-//       />
-//     </div>
-//   );
-// };
 
 const ImgComponent = () => {
   return (
     <div>
-      <img src="/pjHomeCover.svg" alt="Your UI" className="mt-4" />
+      <img src="/PJ-HOMEPAGE.png" alt="Your UI" className="mt-4" />
     </div>
   );
 };
